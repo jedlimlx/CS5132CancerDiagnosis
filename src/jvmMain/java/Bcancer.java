ae28f621481ed5a8a5ad1179688fac4409619988
MatthewKan2005 <31413688+MatthewKan2005@users.noreply.github.com>
2022-09-25 20:39:25 +0800

    Updated rules to reflect their fuzzy nature

--- src/jvmMain/java/Bcancer.java
+++ src/jvmMain/java/Bcancer.java
@@ -27,12 +27,12 @@ public class Bcancer
         Area.setEnabled(true); //Sets whether the rule is enabled. An enabled rule will be fired, but a disabled rule will not.
         Area.setRange(185, 4255); //Sets the range of the variable between [minimum, maximum]
         Area.setLockValueInRange(false);
-        Area.addTerm(new Trapezoid("Smaller", 184.5, 185.0, 748.8, 1000, 1.0)); //creates a membership function
-        Area.addTerm(new Trapezoid("Larger", 508.1, 2194, 4255, 4256, 1.0));
+        Area.addTerm(new Trapezoid("Within_Smaller_Trapezoid", 184.5, 185.0, 748.8, 1000, 1.0)); //creates a membership function
+        Area.addTerm(new Trapezoid("Within_Larger_Trapezoid", 508.1, 2194, 4255, 4256, 1.0));
 
         Scanner s = new Scanner(System.in);
         System.out.print("Please enter the input value for Area: ");
-        Area.setValue(10) ; //Double.parseDouble(s.nextLine()) );
+        Area.setValue( Double.parseDouble(s.nextLine()) );
 
         engine.addInputVariable(Area);
 
@@ -42,11 +42,11 @@ public class Bcancer
         Perimeter.setEnabled(true); //Sets whether the rule is enabled. An enabled rule will be fired, but a disabled rule will not.
         Perimeter.setRange(50, 252); //Sets the range of the variable between [minimum, maximum]
         Perimeter.setLockValueInRange(false);
-        Perimeter.addTerm(new Trapezoid("Smaller", 49.5, 50.1, 92.58, 103, 1.0)); //creates a membership function
-        Perimeter.addTerm(new Trapezoid("Larger", 85.1, 159.8, 252, 252.5, 1.0));
+        Perimeter.addTerm(new Trapezoid("Within_Smaller_Trapezoid", 49.5, 50.1, 92.58, 103, 1.0)); //creates a membership function
+        Perimeter.addTerm(new Trapezoid("Within_Larger_Trapezoid", 85.1, 159.8, 252, 252.5, 1.0));
 
         System.out.print("Please enter the input value for Perimeter: ");
-        Perimeter.setValue(10); //Double.parseDouble(s.nextLine()) );
+        Perimeter.setValue( Double.parseDouble(s.nextLine()) );
 
         engine.addInputVariable(Perimeter);
 
@@ -56,11 +56,11 @@ public class Bcancer
         Homogeneity.setEnabled(true); //Sets whether the rule is enabled. An enabled rule will be fired, but a disabled rule will not.
         Homogeneity.setRange(0.01, 0.45); //Sets the range of the variable between [minimum, maximum]
         Homogeneity.setLockValueInRange(false);
-        Homogeneity.addTerm(new Trapezoid("More", 0, 0.01, 0.1232, 0.19, 1.0)); //creates a membership function
-        Homogeneity.addTerm(new Trapezoid("Less", 0.0295, 0.2168, 0.45, 0.5, 1.0));
+        Homogeneity.addTerm(new Trapezoid("Within_More_Trapezoid", 0, 0.01, 0.1232, 0.19, 1.0)); //creates a membership function
+        Homogeneity.addTerm(new Trapezoid("Within_Less_Trapezoid", 0.0295, 0.2168, 0.45, 0.5, 1.0));
 
         System.out.print("Please enter the input value for Homogeneity: ");
-        Homogeneity.setValue(10); // Double.parseDouble(s.nextLine()) );
+        Homogeneity.setValue( Double.parseDouble(s.nextLine()) );
 
         engine.addInputVariable(Homogeneity);
 
@@ -70,11 +70,11 @@ public class Bcancer
         Uniformity.setEnabled(true);
         Uniformity.setRange(0.000, 12.000);
         Uniformity.setLockValueInRange(false);
-        Uniformity.addTerm(new Trapezoid("More", -0.5, 0, 1.669, 2.6, 1.0)); //creates a membership function
-        Uniformity.addTerm(new Trapezoid("Less", 0.65, 6.205, 12, 12.5, 1.0));
+        Uniformity.addTerm(new Trapezoid("Within_More_Trapezoid", -0.5, 0, 1.669, 2.6, 1.0)); //creates a membership function
+        Uniformity.addTerm(new Trapezoid("Within_Less_Trapezoid", 0.65, 6.205, 12, 12.5, 1.0));
 
         System.out.print("Please enter the input value for Uniformity: ");
-        Uniformity.setValue(10); // Double.parseDouble(s.nextLine()) );
+        Uniformity.setValue( Double.parseDouble(s.nextLine()) );
 
         engine.addInputVariable(Uniformity);
 
@@ -88,9 +88,9 @@ public class Bcancer
         Diagnosis.setDefuzzifier(new Centroid(200));
         Diagnosis.setDefaultValue(Double.NaN);
         Diagnosis.setLockPreviousValue(false);
-        Diagnosis.addTerm(new Trapezoid("Benign", -0.5, 0, 0.4, 0.5, 1.0)); //creates a membership function
-        Diagnosis.addTerm(new Trapezoid("Malignant", 0.6, 0.7, 1, 1.5, 1.0));
-        Diagnosis.addTerm(new Triangle("Undefined", 0.5, 0.55, 0.6, 1.0));
+        Diagnosis.addTerm(new Trapezoid("Within_Benign_Triangle", -0.5, 0, 0.4, 0.5, 1.0)); //creates a membership function
+        Diagnosis.addTerm(new Trapezoid("Within_Malignant_Triangle", 0.6, 0.7, 1, 1.5, 1.0));
+        Diagnosis.addTerm(new Triangle("Within_Undefined_Triangle", 0.5, 0.55, 0.6, 1.0));
 
         engine.addOutputVariable(Diagnosis);
 
@@ -103,22 +103,27 @@ public class Bcancer
         ruleBlock.setImplication(new Minimum());
         ruleBlock.setActivation(new General()); //the general class is a ruleBlock activation method that activates every rule following the order in which the rules were added to the rule block
 
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Smaller and Homogeneity is More and Uniformity is More) then Diagnosis is Benign", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Smaller and Homogeneity is More and Uniformity is Less) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Smaller and Homogeneity is Less and Uniformity is More) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Smaller and Homogeneity is Less and Uniformity is Less) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Larger and Homogeneity is More and Uniformity is More) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Larger and Homogeneity is More and Uniformity is Less) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Larger and Homogeneity is Less and Uniformity is More) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Smaller and Perimeter is Larger and Homogeneity is Less and Uniformity is Less) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Smaller and Homogeneity is More and Uniformity is More) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Smaller and Homogeneity is More and Uniformity is Less) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Smaller and Homogeneity is Less and Uniformity is More) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Smaller and Homogeneity is Less and Uniformity is Less) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Larger and Homogeneity is More and Uniformity is More) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Larger and Homogeneity is More and Uniformity is Less) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Larger and Homogeneity is Less and Uniformity is More) then Diagnosis is Undefined", engine));
-        ruleBlock.addRule(Rule.parse("if (Area is Larger and Perimeter is Larger and Homogeneity is Less and Uniformity is Less) then Diagnosis is Malignant", engine));
+
+        /** AS THE RULES ARE FUZZY, A PATIENT WILL TRIGGER MORE THAN 1 RULE.
+            IN THE WORST CASE, A PATIENT CAN TRIGGER ALL 16 RULES.
+            HENCE, A FUZZY LOGIC SYSTEM IS NEEDED.
+        */
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Benign_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Smaller_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Smaller_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_More_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_More_Trapezoid) then Diagnosis is Within_Undefined_Triangle", engine));
+        ruleBlock.addRule(Rule.parse("if (Area is Within_Larger_Trapezoid and Perimeter is Within_Larger_Trapezoid and Homogeneity is Within_Less_Trapezoid and Uniformity is Within_Less_Trapezoid) then Diagnosis is Within_Malignant_Triangle", engine));
 
         engine.addRuleBlock(ruleBlock);
 
