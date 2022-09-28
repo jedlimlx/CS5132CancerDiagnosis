ded27e0e8aa007bb480bf5ec90f2705efc4792b6
waffleSheep <zayan.superboy@gmail.com>
2022-09-28 22:25:36 +0800

    more decision

---
+++ Test/DecisionTestProject/src/DataFrame.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+// DataFrame Class to hold data
+public class DataFrame {
+    private final String[][] data;
+    private final String[] labels;
+    private final HashMap<String, Integer> labelToColumnHashMap;
+    private final int attributeCount, sampleCount;
+    private double targetEntropy;
+
+    public DataFrame(String[][] data, String[] labels, HashMap<String, Integer> labelToColumnHashMap, int attributeCount, int sampleCount){
+        this.data = data;
+        this.labels = labels;
+        this.labelToColumnHashMap = labelToColumnHashMap;
+        this.attributeCount = attributeCount;
+        this.sampleCount = sampleCount;
+    }
+
+    public DataFrame(String csvName, int attributeCount, int sampleCount) throws FileNotFoundException {
+        this.data = new String[attributeCount][sampleCount];
+        this.labels = new String[attributeCount];
+        this.labelToColumnHashMap = new HashMap<String, Integer>();
+        Scanner sc = new Scanner(new File("PlayTennis.csv"));
+        String[] stringVars = sc.nextLine().split(",");
+        for(int i = 0; i < attributeCount; ++i){
+            labels[i] = stringVars[i];
+            labelToColumnHashMap.put(labels[i], i);
+        }
+        for(int i = 0; i < sampleCount; ++i){
+            stringVars = sc.nextLine().split(",");
+            for(int j = 0; j < attributeCount; ++j)
+                data[j][i] = stringVars[j];
+        }
+
+        targetEntropy = 0.0;
+        HashMap<String, Integer> hashTable = new HashMap<String, Integer>();
+        for(String i : data[attributeCount-1]) {
+            if(hashTable.containsKey(i))
+                hashTable.put(i, hashTable.get(i) + 1);
+            else
+                hashTable.put(i, 0);
+        }
+        for(Map.Entry<String, Integer> i: hashTable.entrySet()){
+            double prob = ((double) i.getValue())/sampleCount;
+            targetEntropy -= prob * Math.log(prob)/Math.log(2);
+        }
+
+        this.attributeCount = attributeCount;
+        this.sampleCount = sampleCount;
+    }
+
+    public String[][] getData() {
+        return data;
+    }
+
+    public int getAttributeCount() {
+        return attributeCount;
+    }
+
+    public String[] getLabels() {
+        return labels;
+    }
+
+    public HashMap<String, Integer> getLabelToColumnHashMap() {
+        return labelToColumnHashMap;
+    }
+
+    public int getSampleCount() {
+        return sampleCount;
+    }
+
+    public double getTargetEntropy() {
+        return targetEntropy;
+    }
+
+    public String[] getTargetData(){
+        return data[attributeCount-1];
+    }
+
+    public int getColumnFromLabel(String label){
+        return labelToColumnHashMap.get(label);
+    }
+}
--- Test/DecisionTestProject/src/DecisionTree.java
+++ Test/DecisionTestProject/src/DecisionTree.java
@@ -1,5 +1,48 @@
-import java.lang.Math;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DecisionTree {
+    public double calculateInformationGain(DataFrame dataFrame, String label){
+        int dataColumn = dataFrame.getColumnFromLabel(label);
+        String[] dataColumnClasses = getUniqueValues(dataFrame.getData()[dataColumn]);
+        HashMap<String, Integer> classHashTable = getEmptyHash(dataColumnClasses);
+        for(String i : dataFrame.getData()[dataColumn])
+            classHashTable.put(i, classHashTable.get(i) + 1);
+        double informationGain = dataFrame.getTargetEntropy();
+        for(Map.Entry<String, Integer> i: classHashTable.entrySet())
+            informationGain -= ((double) i.getValue())/dataFrame.getSampleCount() * calculateEntropy(dataFrame, i.getKey());
+        return informationGain;
+    }
+    public double calculateEntropy(DataFrame dataFrame, String label){
+        HashMap<String, Integer> hashTable = getEmptyHash(dataFrame.getTargetData());
+        int dataColumn = dataFrame.getColumnFromLabel(label);
+        int count = 0;
+        for(int i = 0; i < dataFrame.getSampleCount(); ++i) {
+            if (dataFrame.getData()[dataColumn][i].equals(label)) {
+                hashTable.put(dataFrame.getTargetData()[i], hashTable.get(dataFrame.getTargetData()[i]) + 1);
+                count++;
+            }
+        }
+
+        double entropy = 0;
+        for(Map.Entry<String, Integer> i: hashTable.entrySet()){
+            if(i.getValue() != 0) {
+                double prob = ((double) i.getValue()) / count;
+                entropy -= prob * Math.log(prob) / Math.log(2);
+            }
+        }
+        return entropy;
+    }
+
+    public String[] getUniqueValues(String[] array){
+        return Arrays.stream(array).distinct().toArray(String[]::new);
+    }
+
+    public HashMap<String, Integer> getEmptyHash(String[] array){
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+        for(String i : array)
+            hashMap.put(i, 0);
+        return hashMap;
+    }
 }
