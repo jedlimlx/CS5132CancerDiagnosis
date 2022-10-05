161d36e7815b2f60f89f5c2ceae60acdb630fa96
waffleSheep <zayan.superboy@gmail.com>
2022-10-05 15:40:57 +0800

    more RFC

--- src/jvmMain/java/RandomForestClassifier.java
+++ src/jvmMain/java/RandomForestClassifier.java
@@ -1,19 +1,22 @@
 public class RandomForestClassifier {
     public int max_depth;
     public int min_samples_split;
-
-    DecisionTree[] decisionTrees;
-    public RandomForestClassifier(int max_depth, int min_samples_split){
+    public int num_trees;
+    public DecisionTree[] decisionTrees;
+    public RandomForestClassifier(int max_depth, int min_samples_split, int num_trees){
         this.max_depth = max_depth;
         this.min_samples_split = min_samples_split;
-    }
-
-    public RandomForestClassifier(int max_depth){
-        this(max_depth, 2);
+        this.num_trees = num_trees;
+        this.decisionTrees = new DecisionTree[num_trees];
     }
 
     public RandomForestClassifier(){
-        this(100, 2);
+        this(100, 2, 10);
     }
+
+    public void fit(double[][] X, int[] Y){
+
+    }
+
 }
 
