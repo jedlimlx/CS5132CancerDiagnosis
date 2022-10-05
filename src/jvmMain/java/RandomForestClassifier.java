d793ee30314d344f1b212b6aeef578179ce01813
waffleSheep <zayan.superboy@gmail.com>
2022-10-05 15:11:36 +0800

    RFC constructors done

--- src/jvmMain/java/RandomForestClassifier.java
+++ src/jvmMain/java/RandomForestClassifier.java
@@ -1,2 +1,19 @@
 public class RandomForestClassifier {
+    public int max_depth;
+    public int min_samples_split;
+
+    DecisionTree[] decisionTrees;
+    public RandomForestClassifier(int max_depth, int min_samples_split){
+        this.max_depth = max_depth;
+        this.min_samples_split = min_samples_split;
+    }
+
+    public RandomForestClassifier(int max_depth){
+        this(max_depth, 2);
+    }
+
+    public RandomForestClassifier(){
+        this(100, 2);
+    }
 }
+
